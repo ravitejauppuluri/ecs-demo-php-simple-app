6f22aa474c240dbcbf0d53ee07721e489e0e6a01
ravitejauppuluri <47230412+ravitejauppuluri@users.noreply.github.com>
2019-02-01 11:17:50 +1100

    Update index.php

--- src/index.php
+++ src/index.php
@@ -16,7 +16,7 @@
         <div class="container">
             <div class="hero-unit">
                 <h1>Telstra CI/CD</h1>
-                <h2>Congratulations</h2>
+                <h2>Congratulations Again</h2>
                 <p>Your Telstra application is now running on a Docker container in Amazon ECS.</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
                 <?php
